e853738b5c3d26f96c463709762f05f40dc004d7
Constantine Leshkov <leshkov@hardpoint.eu>
2024-09-06 21:56:16 +0300

    5

--- main.tf
+++ main.tf
@@ -3,21 +3,27 @@ terraform {
 
   backend "s3" {
     endpoints = {
-      s3 = "https://fra1.digitaloceanspaces.com"  # Регион вашего Space
+      s3 = "https://fra1.digitaloceanspaces.com"
     }
-    bucket = "tf2-state-store"                   # Имя вашего Space
-    key    = "terraform.tfstate"                 # Имя файла состояния
-    region = "us-east-1"                         # Необязательно, стандартная для S3 совместимости
+    bucket = "tf2-state-store"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
 
-    # Отключаем AWS-специфические проверки, так как используем DigitalOcean Spaces
     skip_credentials_validation = true
     skip_requesting_account_id  = true
     skip_metadata_api_check     = true
     skip_region_validation      = true
     skip_s3_checksum            = true
   }
+
+  required_providers {
+    digitalocean = {
+      source  = "digitalocean/digitalocean"
+      version = "~> 2.0"
+    }
+  }
 }
 
 provider "digitalocean" {
-  token = var.do_token  # Используем API токен DigitalOcean
+  token = var.do_token
 }
