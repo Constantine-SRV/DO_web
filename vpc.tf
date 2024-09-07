3a980497ae1a6f56a89c30437782f27a231a1e72
Constantine Leshkov <leshkov@hardpoint.eu>
2024-09-07 22:36:52 +0300

    25

---
+++ vpc.tf
@@ -0,0 +1,8 @@
+# Create a VPC with a custom IP range
+resource "digitalocean_vpc" "vpc-0-0" {
+  name     = "vpc-0-0"      # Updated name to use only valid characters (letters, numbers, hyphens)
+  region   = "fra1"         # Region for VPC (e.g., Frankfurt)
+  ip_range = "10.10.0.0/16" # CIDR block for VPC
+
+  description = "VPC for Terraform Droplet"
+}
