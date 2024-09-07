1d1eb2b8e8e7ac8c926cdfb7a6b9b458448cc249
Constantine Leshkov <leshkov@hardpoint.eu>
2024-09-07 12:08:29 +0300

    13

--- vm.tf
+++ vm.tf
@@ -1,23 +1,19 @@
-provider "digitalocean" {
-  token = var.do_token # Use DigitalOcean API token
-}
-
-# Создаем VPC с диапазоном IP адресов
+# Create a VPC with a custom IP range
 resource "digitalocean_vpc" "vpc_0_0" {
-  name     = "0_0_VPC"
-  region   = "fra1"         # Region for VPC (e.g., Frankfurt)
-  ip_range = "10.10.0.0/16" # CIDR block for VPC
-
+  name   = "0_0_VPC"
+  region = "fra1"  # Region for VPC (e.g., Frankfurt)
+  ip_range = "10.10.0.0/16"  # CIDR block for VPC
+  
   description = "VPC for Terraform Droplet"
 }
 
-# Создаем Droplet в указанной VPC
+# Create a Droplet in the specified VPC
 resource "digitalocean_droplet" "vm_0_0" {
-  name     = "terraform-droplet"
-  region   = "fra1"                      # Region for Droplet (e.g., Frankfurt)
-  size     = "s-1vcpu-1gb"               # Droplet size
-  image    = "ubuntu-22-04-x64"          # OS image for the Droplet
-  vpc_uuid = digitalocean_vpc.vpc_0_0.id # Attach Droplet to created VPC
+  name   = "terraform-droplet"
+  region = "fra1"  # Region for Droplet (e.g., Frankfurt)
+  size   = "s-1vcpu-1gb"  # Droplet size
+  image  = "ubuntu-22-04-x64"  # OS image for the Droplet
+  vpc_uuid = digitalocean_vpc.vpc_0_0.id  # Attach Droplet to the created VPC
 
   # Use the existing SSH key
   ssh_keys = [data.digitalocean_ssh_key.az_ssh_key.id]
@@ -25,12 +21,12 @@ resource "digitalocean_droplet" "vm_0_0" {
   tags = ["terraform", "droplet"]
 }
 
-# Fetch the existing SSH key
+# Fetch the existing SSH key by name
 data "digitalocean_ssh_key" "az_ssh_key" {
-  name = "az_ssh_key_pem" # Name of your existing SSH key
+  name = "az_ssh_key_pem"  # Name of your existing SSH key
 }
 
-# Output to get the public IP of the Droplet
+# Output the public IP of the Droplet
 output "droplet_ip" {
   description = "The public IP address of the Droplet"
   value       = digitalocean_droplet.vm_0_0.ipv4_address
