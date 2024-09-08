9b049381fa22a8ebd7e5a9beff1c6e7a998755f0
Constantine Leshkov <leshkov@hardpoint.eu>
2024-09-08 10:19:44 +0300

    36

--- restore_pg_dump.sh
+++ restore_pg_dump.sh
@@ -12,7 +12,7 @@ echo "!-apt-get update"
 sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
 sudo apt-get update
-sudo apt-get install -y postgresql-client-16
+sudo apt-get install -y postgresql-client
 
 echo "!- Setup az-cli get"
 sudo apt-get install azure-cli -y
